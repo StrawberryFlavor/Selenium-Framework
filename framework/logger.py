386c942014b7d303b429b14bffc73f0375fe0461
wind <h-zhaoyi@foxmail.com>
2018-05-03 15:26:11 +0800

    update

--- framework/logger.py
+++ framework/logger.py
@@ -17,10 +17,9 @@ class Logger(object):
 
         # 创建一个handler，用于写入日志文件
         rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
-        # log_path = os.path.dirname(os.getcwd()) + '/Logs/'  # 项目根目录下/Logs 保存日志
         log_path = os.path.dirname(os.path.abspath('.')) + '/logs/'
-        # 如果case组织结构式 /testsuit/featuremodel/xxx.py ， 那么得到的相对路径的父路径就是项目根目录
         log_name = log_path + rq + '.log'
+
         fh = logging.FileHandler(log_name)
         fh.setLevel(logging.INFO)
 
